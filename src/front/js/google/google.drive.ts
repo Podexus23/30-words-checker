abdadf41b9c2f8e44e4c4ebc1d284bef11d3dcae
Podexus23 <santon23@gmail.com>
2025-01-10 01:34:20 +0300

    feat: add creating and reading data from google drive

--- src/front/js/google/google.drive.ts
+++ src/front/js/google/google.drive.ts
@@ -29,11 +29,12 @@ async function loadGoogleApi() {
 
 let client: any;
 let access_token: any;
+let fileId: string;
 
 function initClient() {
   client = google.accounts.oauth2.initTokenClient({
     client_id: CLIENT_ID,
-    scope: "https://www.googleapis.com/auth/drive.readonly",
+    scope: "https://www.googleapis.com/auth/drive",
     callback: (tokenResponse: any) => {
       access_token = tokenResponse.access_token;
       console.log(tokenResponse);
@@ -46,7 +47,7 @@ function getToken() {
 }
 
 export const params = new URLSearchParams({
-  q: "name='words.json'",
+  q: "name='words1.json'",
   fields: "files(id, name)",
   pageSize: "10",
   key: USER_API,
@@ -64,6 +65,86 @@ async function loadData() {
   );
   const data = await xhr.json();
   console.log(data);
+
+  fileId = data.files[0].id as string;
+  console.log(fileId);
+}
+
+async function loadDataJson(fileId: string) {
+  const xhr = await fetch(
+    `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + access_token,
+      },
+    },
+  );
+  console.log(`xhr`);
+
+  const data = await xhr.json();
+  console.log(data);
+}
+
+async function createDataJson() {
+  const metadata = {
+    name: "words1.json",
+    mimeType: "application/json",
+  };
+
+  const fileContent = new Blob([JSON.stringify({ cat: "kotek" })], {
+    type: "application/json",
+  });
+  console.log(fileContent);
+
+  const formData = new FormData();
+
+  formData.append(
+    "metadata",
+    new Blob([JSON.stringify(metadata)], { type: "application/json" }),
+  );
+  formData.append("file", fileContent);
+
+  const xhr = await fetch(
+    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+    {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + access_token,
+      },
+      body: formData,
+    },
+  );
+  if (xhr.ok) {
+    const data = await xhr.json();
+    console.log("File created with ID:", data.id);
+    console.log(`${data}`);
+  } else {
+    console.error("Error creating file:", await xhr.text());
+  }
+}
+
+async function updateDataJson(fileId: string) {
+  const xhr = await fetch(
+    `https://www.googleapis.com/drive/v3/files/${fileId}`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer " + access_token,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        cat: "mopet",
+        dog: "chebupet",
+      }),
+    },
+  );
+  if (xhr.ok) {
+    const data = await xhr.json();
+    console.log("File created with ID:", data.id);
+  } else {
+    console.error("Error creating file:", await xhr.text());
+  }
 }
 
 export function renderGoogleBlock() {
@@ -79,10 +160,26 @@ export function renderGoogleBlock() {
     className: "google-request",
     textContent: "Request",
   });
+
   const getDataButton = createTag({
     tagName: "button",
     className: "google-data",
     textContent: "Get data",
+  });
+  const requestDataButton = createTag({
+    tagName: "button",
+    className: "google-request-json",
+    textContent: "Get json",
+  });
+  const createDataButton = createTag({
+    tagName: "button",
+    className: "google-create-json",
+    textContent: "Create json",
+  });
+  const updateDataButton = createTag({
+    tagName: "button",
+    className: "google-update-json",
+    textContent: "Update json",
   });
   requestButton.addEventListener("click", () => {
     getToken();
@@ -90,7 +187,19 @@ export function renderGoogleBlock() {
   getDataButton.addEventListener("click", () => {
     loadData();
   });
+  requestDataButton.addEventListener("click", () => {
+    loadDataJson(fileId);
+  });
+  createDataButton.addEventListener("click", () => {
+    createDataJson();
+  });
+  updateDataButton.addEventListener("click", () => {
+    updateDataJson(fileId);
+  });
   googleBlock.append(requestButton);
   googleBlock.append(getDataButton);
+  googleBlock.append(requestDataButton);
+  googleBlock.append(createDataButton);
+  googleBlock.append(updateDataButton);
   return googleBlock;
 }
